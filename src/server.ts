6853f19f593310e739384f01c36a08357af39bb7
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-19 18:51:32 -0300

    Adição de arquivos iniciais do projeto
    Criação do servidor, bem como uma rota GET e uma rota POST, além da instalação das dependências do projeto e configuração do ambiente

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express, { request, response } from "express";
+
+const app = express();
+
+/* MÉTODOS HTTP QUE VAMOS USAR
+  GET = busca
+  POST = criação
+  PUT = alteração
+  DELETE = exclusão
+  PATCH = alterar informação específica
+*/
+
+app.get("/", (request, response) => {
+  return response.json({
+    message: "Olá, NLW 05!"
+  })
+})
+
+app.post("/", (request, response) => {
+  return response.json({
+    message: "Usuário salvo com sucesso!"
+  })
+})
+
+app.listen(3333, () => console.log("Servidor rodando na porta 3333"))
