e3e0032befee051beafa4c020abd89ee3b0f2405
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-22 11:04:42 -0300

    Cadastro das mensagens a partir da conexão do usuário
    Cadastro no banco de dados os conteúdos das mensagens enviadas a partir da conexão criada

--- src/websockets/client.ts
+++ src/websockets/client.ts
@@ -1,15 +1,23 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
 import { UsersService } from "../services/UsersService";
+import { MessagesService } from "../services/MessagesService";
+
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionsService();
   const usersService = new UsersService();
+  const messagesService = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
 
-    const { text, email } = params;
+    const { text, email } = params as IParams;
+    let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
 
@@ -22,7 +30,11 @@ io.on("connect", (socket) => {
         socket_id,
         user_id: user.id,
       });
+
+      user_id = user.id;
     } else {
+      user_id = userExists.id;
+
       const connection = await connectionsService.findByUserId(userExists.id);
 
       if (!connection) {
@@ -37,5 +49,10 @@ io.on("connect", (socket) => {
         await connectionsService.create(connection);
       }
     }
+
+    await messagesService.create({
+      text,
+      user_id,
+    });
   });
 });
