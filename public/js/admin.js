0fa0719a2fa1e3312a00f9cf96c162a73990d5f0
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-04-23 10:00:15 -0300

    Visualização das mensagens enviadas pelos clientes
    Criação de funcionalidade que mostra as mensagens que os clientes enviaram para que o atendente possa responder

--- public/js/admin.js
+++ public/js/admin.js
@@ -30,4 +30,36 @@ function call(id) {
   });
 
   document.getElementById("supports").innerHTML += rendered;
+
+  const params = {
+    user_id: connection.user_id,
+  };
+
+  socket.emit("admin_list_messages_by_user", params, (messages) => {
+    const divMessages = document.getElementById(
+      `allMessages${connection.user_id}`
+    );
+    messages.forEach((message) => {
+      const createDiv = document.createElement("div");
+
+      if (message.admin_id === null) {
+        createDiv.className = "admin_message_client";
+
+        createDiv.innerHTML = `<span>${connection.user.email}</span>`;
+        createDiv.innerHTML += `<span>${message.text}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}`;
+      } else {
+        createDiv.className = "admin_message_admin";
+
+        createDiv.innerHTML = `Atendente: <span>${message.text}</span>`;
+        createDiv.innerHTML += `<span class="admin_date">${dayjs(
+          message.created_at
+        ).format("DD/MM/YYYY HH:mm:ss")}`;
+      }
+
+      divMessages.appendChild(createDiv);
+    });
+  });
 }
